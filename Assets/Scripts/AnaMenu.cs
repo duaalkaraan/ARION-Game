946a1beb99cd56a57ea15cdaccebf85f2d4882ed
nevalcolak0-bit <nevalcolak0@gmail.com>
2026-04-12 00:27:27 +0300

    Ana menü yapıldı

--- Assets/Scripts/AnaMenu.cs
+++ Assets/Scripts/AnaMenu.cs
@@ -3,17 +3,52 @@ using UnityEngine.SceneManagement;
 
 public class AnaMenu : MonoBehaviour
 {
-    // Maceraya Baþla butonu için
+    [Header("Paneller")]
+    public GameObject onayPaneli;    // "Rafi esir" yazan çýkýþ onay paneli
+    public GameObject ayarlarPaneli; // Eðer yaptýysan ayarlar paneli
+
+    // 1. MACERAYA BAÞLA BUTONU ÝÇÝN
     public void OyunuBaslat()
     {
-        // "OyunSahnesi" yazan yere yüklemek istediðin sahnenin adýný yaz
+        // "SampleScene" yazan yere kendi oyun sahnenin adýný týrnak içinde yaz!
         SceneManager.LoadScene("SampleScene");
     }
 
-    // Çýkýþ butonu için
-    public void OyundanCik()
+    // 2. VEDA ET BUTONU ÝÇÝN (Paneli açar)
+    public void OnayPaneliniAc()
     {
-        Debug.Log("Oyundan çýkýldý!");
+        onayPaneli.SetActive(true);
+        if (ayarlarPaneli != null) ayarlarPaneli.SetActive(false); // Ayarlar açýksa kapatýr
+    }
+
+    // 3. HAYIR / VAZGEÇ BUTONU ÝÇÝN (Paneli kapatýr)
+    public void Vazgec()
+    {
+        onayPaneli.SetActive(false);
+    }
+
+    // 4. EVET / ÇIKIÞ BUTONU ÝÇÝN (Oyunu kapatýr)
+    public void TamamenCik()
+    {
+        Debug.Log("Oyun kapatýlýyor... (Bu yazý Console'da çýkýyorsa kod çalýþýyor demektir)");
+
+        // Gerçek oyun dosyasýnda (Build) kapatýr
         Application.Quit();
+
+        // Unity Editor içindeyken Play modunu durdurur (Test etmek için)
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    // 5. AYARLAR BUTONU ÝÇÝN (Opsiyonel)
+    public void AyarlariAc()
+    {
+        ayarlarPaneli.SetActive(true);
+    }
+
+    public void AyarlariKapat()
+    {
+        ayarlarPaneli.SetActive(false);
     }
 }
